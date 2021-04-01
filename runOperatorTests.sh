e3216bacec02a725a60419f1da435fe9c6d5e9a9
Patrick Stephens <patrick.stephens@couchbase.com>
2021-04-01 16:14:16 +0100

    Tweaks to pick up new image/repo names

--- runOperatorTests.sh
+++ runOperatorTests.sh
@@ -2,7 +2,7 @@
 set -eux
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OPERATOR_REPO_DIR=$(find $SCRIPT_DIR/../ -type d -name "couchbase-operator" -print0)
-LOGSHIPPER_REPO_DIR=$(find $SCRIPT_DIR/../ -type d -name "couchbase-operator-logging" -print0)
+LOGSHIPPER_REPO_DIR=$(find $SCRIPT_DIR/../ -type d -name "couchbase-fluent-bit" -print0)
 
 DOCKER_TAG=${DOCKER_TAG:-v1}
 SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:6.6.1}
@@ -23,13 +23,13 @@ if [[ "${RECREATE_CLUSTER}" == "yes" ]]; then
 fi
 kind load docker-image "couchbase/couchbase-operator:${DOCKER_TAG}"
 kind load docker-image "couchbase/couchbase-operator-admission:${DOCKER_TAG}"
-kind load docker-image "couchbase/operator-logging:${DOCKER_TAG}"
-kind load docker-image "couchbase/operator-logging-rhel:${DOCKER_TAG}"
+kind load docker-image "couchbase/fluent-bit:${DOCKER_TAG}"
+kind load docker-image "couchbase/fluent-bit:${DOCKER_TAG}"
 
 # Not strictly required but improves caching performance
 docker pull "${SERVER_IMAGE}"
 kind load docker-image "${SERVER_IMAGE}"
 
 pushd "${OPERATOR_REPO_DIR}"
-go test github.com/couchbase/couchbase-operator/test/e2e -run TestOperator -v --race -timeout 24h -parallel 1 -args -color --logging-image "couchbase/operator-logging:${DOCKER_TAG}" "$@"
+go test github.com/couchbase/couchbase-operator/test/e2e -run TestOperator -v --race -timeout 24h -parallel 1 -args -color --logging-image "couchbase/fluent-bit:${DOCKER_TAG}" "$@"
 popd
