fea06d897768258f2076f9fcc364d06a94ca3091
Patrick Stephens <patrick.stephens@couchbase.com>
2021-04-27 14:12:46 +0100

    Update to fix paths

--- gitpod/fluent-entrypoint.sh
+++ gitpod/fluent-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-FLUENT_BIT_HOME=${FLUENT_BIT_HOME:-/opt/couchbase/bin/tools/fluent-bit}
+FLUENT_BIT_HOME=${FLUENT_BIT_HOME:-/fluent-bit}
 "${FLUENT_BIT_HOME}/bin/fluent-bit" -c "${FLUENT_BIT_HOME}/etc/fluent-bit.conf" &
 
 # Run the original entrypoint
