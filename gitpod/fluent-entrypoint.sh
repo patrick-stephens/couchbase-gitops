2d8606a5c8977180265e41babe222616a0491eee
Patrick Stephens <patrick.stephens@couchbase.com>
2021-04-27 15:19:53 +0100

    Switch to example image

--- gitpod/fluent-entrypoint.sh
+++ gitpod/fluent-entrypoint.sh
@@ -4,5 +4,4 @@ set -ex
 FLUENT_BIT_HOME=${FLUENT_BIT_HOME:-/fluent-bit}
 "${FLUENT_BIT_HOME}/bin/fluent-bit" -c "${FLUENT_BIT_HOME}/etc/fluent-bit.conf" &
 
-# Run the original entrypoint
-/bin/bash /entrypoint.sh "$@"
+/opt/couchbase/start-cb.sh && sleep 20
