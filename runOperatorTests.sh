b32d09de278ee64173b93269155bc370d1f04060
Patrick Stephens <patrick.stephens@couchbase.com>
2021-04-27 14:57:39 +0100

    Update to latest

--- runOperatorTests.sh
+++ runOperatorTests.sh
@@ -5,7 +5,7 @@ OPERATOR_REPO_DIR=$(find $SCRIPT_DIR/../ -type d -name "couchbase-operator" -pri
 LOGSHIPPER_REPO_DIR=$(find $SCRIPT_DIR/../ -type d -name "couchbase-fluent-bit" -print0)
 
 DOCKER_TAG=${DOCKER_TAG:-v1}
-SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:6.6.1}
+SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:6.6.2}
 
 RECREATE_CLUSTER=${RECREATE_CLUSTER:-no}
 
