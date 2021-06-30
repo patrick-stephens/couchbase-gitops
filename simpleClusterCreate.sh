8819dc12d497c2e6a786362a5315a183f224d813
Patrick Stephens <patrick.stephens@couchbase.com>
2021-06-30 10:54:48 +0100

    Added some comments

--- simpleClusterCreate.sh
+++ simpleClusterCreate.sh
@@ -12,9 +12,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Simple script to provision a Kubernetes cluster using KIND: https://kind.sigs.k8s.io/
+# It then spins up a Couchbase Server cluster on it using Helm: https://helm.sh/
+# To use, need Docker (or a container runtime) installed plus KIND & Helm.
 set -eu
 
+# In case you want a different name
 CLUSTER_NAME=${CLUSTER_NAME:-couchbase-test}
+# The server container image to use
 SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:6.6.2}
 
 # Delete the old cluster
@@ -38,13 +44,19 @@ rm -f "${CLUSTER_CONFIG}"
 
 # Add Couchbase via helm chart
 helm repo add couchbase https://couchbase-partners.github.io/helm-charts/
+# Ensure we update the repo (may have added it years ago!)
 helm repo update
+# Always installs the latest version, can be pinned with --version X
 helm upgrade --install couchbase couchbase/couchbase-operator --set cluster.image="${SERVER_IMAGE}"
 
-# Wait for deployment to complete
+# Wait for deployment to complete, potentially a call to --wait may work with helm but it can be flakey
 echo "Waiting for CB to start up..."
+# The operator uses readiness gates to hold the containers until the cluster is actually ready to be used
 until [[ $(kubectl get pods --field-selector=status.phase=Running --selector='app=couchbase' --no-headers 2>/dev/null |wc -l) -eq 3 ]]; do
     echo -n '.'
     sleep 2
 done
-echo "CB started"
+echo "CB configured and ready to go"
+
+# Couchbase cluster is ready to go, not just started but configured.
+# If you just run the container up then it marks itself ready as soon as it starts which is not entirely true.
