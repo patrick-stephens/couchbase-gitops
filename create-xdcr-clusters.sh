6acacec46b4c21d3272e3ef0ed93c4274f071020
Patrick Stephens <patrick.stephens@couchbase.com>
2021-10-29 14:18:06 +0100

    Added initial xdcr set up script

---
+++ create-xdcr-clusters.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+# Copyright 2021 Couchbase, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file  except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the  License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Simple script to provision a Kubernetes cluster using KIND: https://kind.sigs.k8s.io/
+# It then spins up a Couchbase Server cluster on it using Helm: https://helm.sh/
+# To use, need Docker (or a container runtime) installed plus kubectl, KIND & Helm.
+set -eu
+
+CLUSTER_NAME=${CLUSTER_NAME:-kind}
+SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:7.0.2}
+SERVER_COUNT=${SERVER_COUNT:-1}
+
+kind delete cluster --name="${CLUSTER_NAME}"
+kind create cluster --name="${CLUSTER_NAME}" --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+EOF
+
+docker pull "${SERVER_IMAGE}"
+kind load docker-image "${SERVER_IMAGE}" --name="${CLUSTER_NAME}"
+
+# Add Istio for local and remote namespaces in STRICT mode with injection enabled
+TEMPDIR=$(mktemp -d)
+pushd "$TEMPDIR"
+curl -L https://istio.io/downloadIstio | sh -
+pushd istio-*/bin
+
+./istioctl install --set profile=default --skip-confirmation --set values.global.istiod.enableAnalysis=true --set meshConfig.accessLogFile=/dev/stdout --set values.global.proxy.privileged=true
+# https://istio.io/latest/docs/tasks/security/authentication/authn-policy/#globally-enabling-istio-mutual-tls-in-strict-mode
+kubectl apply -f - <<EOF
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: local
+  labels:
+    istio-injection: enabled
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: remote
+  labels:
+    istio-injection: enabled
+---
+apiVersion: "security.istio.io/v1beta1"
+kind: "PeerAuthentication"
+metadata:
+  name: "peer-authentication-cluster"
+  namespace: istio-system
+spec:
+  mtls:
+    mode: STRICT
+---
+apiVersion: security.istio.io/v1beta1
+kind: PeerAuthentication
+metadata:
+  name: peer-authentication-dac
+  namespace: default
+spec:
+  selector:
+    matchLabels:
+      app: couchbase-operator-admission
+  mtls:
+    mode: PERMISSIVE
+---
+apiVersion: security.istio.io/v1beta1
+kind: PeerAuthentication
+metadata:
+  name: peer-authentication-metrics
+  namespace: istio-system
+spec:
+  selector:
+    matchLabels:
+      app: couchbase
+  portLevelMtls:
+    9091:
+      mode: PERMISSIVE
+---
+apiVersion: security.istio.io/v1beta1
+kind: PeerAuthentication
+metadata:
+  name: peer-authentication-certification
+  namespace: default
+spec:
+  selector:
+    matchLabels:
+      app: certification
+  mtls:
+    mode: STRICT
+---
+EOF
+
+popd
+popd
+rm -rf "$TEMPDIR"
+
+# TODO: test with LoadBalancer
+
+helm repo add couchbase https://couchbase-partners.github.io/helm-charts/ || helm repo add couchbase https://couchbase-partners.github.io/helm-charts
+helm repo update
+
+HELM_CONFIG=$(mktemp)
+cat << EOF > "${HELM_CONFIG}"
+cluster:
+    image: ${SERVER_IMAGE}
+    networking:
+        networkPlatform: Istio
+    servers:
+        default:
+            size: ${SERVER_COUNT}
+    security:
+        password: "Password"
+EOF
+
+helm upgrade --install remote couchbase/couchbase-operator --values="${HELM_CONFIG}" --namespace remote
+
+until [[ $(kubectl -ns remote get pods --field-selector=status.phase=Running --selector='app=couchbase' --no-headers 2>/dev/null |wc -l) -eq $SERVER_COUNT ]]; do
+    echo -n '.'
+    sleep 2
+done
+
+CLUSTER_ID=$(kubectl get cbc --namespace remote remote-couchbase-cluster -o template --template='{{.status.clusterId}}')
+cat << EOF >> "${HELM_CONFIG}"
+    xdcr:
+        managed: true
+        remoteClusters:
+            - authenticationSecret: auth-couchbase-couchbase-cluster
+              hostname: couchbase://remote-couchbase-cluster-srv.remote?network=default
+              name: remote-couchbase-cluster
+              uuid: ${CLUSTER_ID}
+EOF
+
+helm upgrade --install local couchbase/couchbase-operator --set install.admissionController=false --values="${HELM_CONFIG}" --namespace local
+
+# Wait for deployment to complete, the --wait flag does not work for this.
+echo "Waiting for CB to start up..."
+# The operator uses readiness gates to hold the containers until the cluster is actually ready to be used
+until [[ $(kubectl -ns local get pods --field-selector=status.phase=Running --selector='app=couchbase' --no-headers 2>/dev/null |wc -l) -eq $SERVER_COUNT ]]; do
+    echo -n '.'
+    sleep 2
+done
+
+echo "CB configured and ready to go"
+
+cat << EOF | kubectl apply -f -
+apiVersion: couchbase.com/v2
+kind: CouchbaseReplication
+metadata:
+  name: replicate-default-buckets
+spec:
+  bucket: default
+  remoteBucket: default
+EOF
+
+echo "Added bucket replication"
