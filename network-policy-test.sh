0544cac945dd5923940ec91a1a0cffdb7ae2ea76
Patrick Stephens <patrick.stephens@couchbase.com>
2021-11-09 09:40:05 +0000

    Lots of experimentation, almost working

--- network-policy-test.sh
+++ network-policy-test.sh
@@ -19,13 +19,16 @@ SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:7.0.2}
 SERVER_COUNT=${SERVER_COUNT:-3}
 NAMESPACE=${NAMESPACE:-test}
 
-# To use NetworkPolicy we need a custom CNI to support it.
-# Here we will use Calico.
+if [[ "${CREATE_CLUSTER:-no}" == "yes" ]]; then
+  # To use NetworkPolicy we need a custom CNI to support it.
+  # Here we will use Calico.
 
-kind delete cluster --name="${CLUSTER_NAME}"
-kind create cluster --name="${CLUSTER_NAME}" --config - <<EOF
+  kind delete cluster --name="${CLUSTER_NAME}"
+  kind create cluster --name="${CLUSTER_NAME}" --config - <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+featureGates:
+ EphemeralContainers: true
 networking:
   disableDefaultCNI: true    # disable kindnet
   podSubnet: 192.168.0.0/16  # set to Calico's default subnet
@@ -34,22 +37,30 @@ nodes:
   - role: worker
 EOF
 
-docker pull "${SERVER_IMAGE}"
-kind load docker-image "${SERVER_IMAGE}" --name="${CLUSTER_NAME}"
+  docker pull "${SERVER_IMAGE}"
+  kind load docker-image "${SERVER_IMAGE}" --name="${CLUSTER_NAME}"
 
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
-kubectl -n kube-system set env daemonset/calico-node FELIX_XDPENABLED=false
-# helm repo add projectcalico https://docs.projectcalico.org/charts || helm repo add projectcalico https://docs.projectcalico.org/charts
-# helm repo update
-# helm install calico projectcalico/tigera-operator --version v3.20.2 --wait
+  kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+  kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
+  kubectl -n kube-system set env daemonset/calico-node FELIX_XDPENABLED=false
+  # helm repo add projectcalico https://docs.projectcalico.org/charts || helm repo add projectcalico https://docs.projectcalico.org/charts
+  # helm repo update
+  # helm install calico projectcalico/tigera-operator --version v3.20.2 --wait
 
-echo "Waiting for Calico pods to start up..."
-until [[ $(kubectl get pods --namespace=kube-system --field-selector=status.phase=Running --no-headers 2>/dev/null | grep -c calico-node) -eq 2 ]]; do
-    echo -n '.'
-    sleep 2
-done
-echo "Calico pods ready to go"
+  echo "Waiting for Calico pods to start up..."
+  until [[ $(kubectl get pods --namespace=kube-system --field-selector=status.phase=Running --no-headers 2>/dev/null | grep -c calico-node) -eq 2 ]]; do
+      echo -n '.'
+      sleep 2
+  done
+  echo "Calico pods ready to go"
+else
+  kubectl delete namespace test
+fi
+
+# We need the K8S server API endpoints so we can monitor and update
+API_SERVER_IP=$(kubectl get endpoints --namespace default kubernetes --output=json|jq '.subsets[0].addresses[0].ip' -r)
+API_SERVER_PORT=$(kubectl get endpoints --namespace default kubernetes --output=json|jq '.subsets[0].ports[0].port' -r)
+echo "K8S API server endpoint is $API_SERVER_IP:$API_SERVER_PORT"
 
 # Add network policy
 # Default deny-all: https://raw.githubusercontent.com/kubernetes/website/main/content/en/examples/service/networking/network-policy-default-deny-all.yaml
@@ -63,19 +74,179 @@ metadata:
 apiVersion: networking.k8s.io/v1
 kind: NetworkPolicy
 metadata:
-  name: default-deny-all
+  name: allow-egress-to-apiserver
+  namespace: $NAMESPACE
+spec:
+  podSelector:
+    matchLabels:
+      app.kubernetes.io/name: couchbase-operator
+  egress:
+  - ports:
+    - port: $API_SERVER_PORT
+      protocol: TCP
+    to:
+    - ipBlock:
+        cidr: $API_SERVER_IP/32
+EOF
+
+# Use a cluster DAC in a non-protected namespace
+helm upgrade --install couchbase-dac couchbase/couchbase-operator --set cluster.image="$SERVER_IMAGE",install.couchbaseCluster=false,install.couchbaseOperator=false --namespace default --wait
+
+# We need the DAC endpoints for the operator to talk to
+DAC_SERVER_IP=$(kubectl get service --namespace default couchbase-dac-couchbase-admission-controller --output=json|jq -r -c '.spec.clusterIP')
+echo "DAC endpoint is $DAC_SERVER_IP:443"
+
+# We have an issue with blocking comms to the DAC prior to running Helm so do afterwards
+
+# Add Couchbase via helm chart but without the DAC
+helm repo add couchbase https://couchbase-partners.github.io/helm-charts/ || helm repo add couchbase https://couchbase-partners.github.io/helm-charts
+helm repo update
+helm upgrade --install couchbase couchbase/couchbase-operator --set cluster.image="$SERVER_IMAGE",install.admissionController=false --namespace "$NAMESPACE"
+
+# Add network policy
+# Default deny-all: https://raw.githubusercontent.com/kubernetes/website/main/content/en/examples/service/networking/network-policy-default-deny-all.yaml
+kubectl apply -f - <<EOF
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $NAMESPACE
+---
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: test-policy
   namespace: $NAMESPACE
 spec:
   podSelector: {}
   policyTypes:
-  - Ingress
-  - Egress
+    - Ingress
+    - Egress
+  ingress:
+    - from:
+        - podSelector: {}
+    - from:
+        - ipBlock:
+            cidr: 0.0.0.0/0
+  egress:
+    - to:
+        - podSelector: {}
+    - to:
+      - namespaceSelector:
+          matchLabels:
+            name: kube-system
+      ports:
+        - port: 53
+          protocol: UDP
+    - to:
+        - ipBlock:
+            cidr: $DAC_SERVER_IP/32
+      # ports:
+      #   - port: 443
+      #     protocol: TCP
+# ---
+# apiVersion: networking.k8s.io/v1
+# kind: NetworkPolicy
+# metadata:
+#   name: default-deny-all
+#   namespace: $NAMESPACE
+# spec:
+#   podSelector: {}
+#   policyTypes:
+#   - Ingress
+#   - Egress
+# ---
+# apiVersion: networking.k8s.io/v1
+# kind: NetworkPolicy
+# metadata:
+#   name: allow-dns-access
+#   namespace: $NAMESPACE
+# spec:
+#   podSelector:
+#     matchLabels: {}
+#   policyTypes:
+#   - Egress
+#   egress:
+#   - to:
+#     - namespaceSelector:
+#         matchLabels:
+#           name: kube-system
+#     ports:
+#     - protocol: UDP
+#       port: 53
+---
+# kind: NetworkPolicy
+# apiVersion: networking.k8s.io/v1
+# metadata:
+#   name: dac-network-policy
+#   namespace: $NAMESPACE
+# spec:
+#   podSelector:
+#     matchLabels:
+#       app.kubernetes.io/name: couchbase-admission-controller
+#   policyTypes:
+#   - Ingress
+#   - Egress
+#   ingress:
+#   - from:
+#     - ipBlock:
+#       cidr: 0.0.0.0/0
+#   - from:
+#     - podSelector:
+#         matchLabels:
+#           app.kubernetes.io/name: couchbase-operator
+#   - from:
+#     - ipBlock:
+#         cidr: $API_SERVER_IP/32
+# ---
+# apiVersion: networking.k8s.io/v1
+# kind: NetworkPolicy
+# metadata:
+#   name: operator-network-policy
+#   namespace: $NAMESPACE
+# spec:
+#   podSelector:
+#     matchLabels:
+#       app.kubernetes.io/name: couchbase-operator
+#   policyTypes:
+#   - Egress
+#   egress:
+#   - to:
+#     - podSelector:
+#         matchLabels:
+#           app.kubernetes.io/name: couchbase-admission-controller
+#   - to:
+#     - podSelector:
+#         matchLabels:
+#           app: couchbase
+# ---
+# apiVersion: networking.k8s.io/v1
+# kind: NetworkPolicy
+# metadata:
+#   name: server-network-policy
+#   namespace: $NAMESPACE
+# spec:
+#   podSelector:
+#     matchLabels:
+#       app: couchbase
+#   policyTypes:
+#   - Ingress
+#   - Egress
+#   egress:
+#   - to:
+#     - podSelector:
+#         matchLabels:
+#           app: couchbase
+#   ingress:
+#   - from:
+#     - podSelector:
+#         matchLabels:
+#           app.kubernetes.io/name: couchbase-operator
+#   - from:
+#     - podSelector:
+#         matchLabels:
+#           app: couchbase
 EOF
-
-# Add Couchbase via helm chart
-helm repo add couchbase https://couchbase-partners.github.io/helm-charts/ || helm repo add couchbase https://couchbase-partners.github.io/helm-charts
-helm repo update
-helm upgrade --install couchbase couchbase/couchbase-operator --set cluster.image="$SERVER_IMAGE" --namespace "$NAMESPACE"
 
 # Wait for deployment to complete, potentially a call to --wait may work with helm but it can be flakey
 echo "Waiting for CB to start up..."
